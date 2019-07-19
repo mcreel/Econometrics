d7edb5a10130bc3283ff6d117bf881d990b12eff
Michael Creel <michael.creel@uab.es>
2019-07-19 09:49:30 +0200

    add quantile regression to npreg

--- src/NP/npreg.jl
+++ src/NP/npreg.jl
@@ -30,7 +30,7 @@ function npreg()
     k = 3 # number of regressors
     Random.seed!(1) # set seed to enable testing
     n = 10000
-    bandwidth = n^(-1.0/(4+k))
+    bandwidth = 0.05
     @show bandwidth
     neval = 100
     x = rand(n,k)*pi*2.0
