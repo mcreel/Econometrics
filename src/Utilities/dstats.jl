d2803d5157dd654b4517e42cc93fbb7cfb88af5b
Michael Creel <michael.creel@uab.es>
2019-07-24 10:22:36 +0200

    add median to dstats

--- src/Utilities/dstats.jl
+++ src/Utilities/dstats.jl
@@ -5,13 +5,10 @@ function dstats(x, rnames="";short=false)
         rnames = rnames'
     end
     m = mean(x,dims=1)
+    mm = median(x,dims=1)
     s = std(x,dims=1)
     sk = m-m
     kt = m-m
-    for i = 1:size(x,2)
-        sk[:,i] .= skewness(x[:,i])
-        kt[:,i] .= kurtosis(x[:,i])
-    end
     mn = minimum(x,dims=1)
     mx = maximum(x,dims=1)
     q05 = fill(0.0,k)
@@ -19,12 +16,12 @@ function dstats(x, rnames="";short=false)
     if short == false
         for i = 1:size(x,2) q05[i] = quantile(x[:,i], 0.05) end
         for i = 1:size(x,2) q95[i] = quantile(x[:,i], 0.95) end
-        cnames = ["  mean", "  std", "skew", "kurt", "min", "max", "q05", "q95"]
-        stats = [m' s' sk' kt' mn' mx' q05 q95] 
+        cnames = ["  mean", " median","  std", "min", "max", "q05", "q95"]
+        stats = [m' mm' s' mn' mx' q05 q95] 
         prettyprint(stats, cnames, rnames)
     else
-        cnames = ["  mean", "  std", "skew", "kurt", "min", "max"]
-        stats = [m' s' sk' kt' mn' mx'] 
+        cnames = ["  mean", " median", "  std", "min", "max"]
+        stats = [m' mm' s' mn' mx'] 
         prettyprint(stats, cnames, rnames)
     end
     return stats
