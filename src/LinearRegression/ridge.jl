1e69ba2ca63e8bf14aab7ac17fdb2860d51a1f54
Michael Creel <michael.creel@uab.es>
2019-08-07 14:13:15 +0200

    snapshot

--- src/LinearRegression/ridge.jl
+++ src/LinearRegression/ridge.jl
@@ -13,17 +13,3 @@ function ridge(y, x, k)
 end
 
 
-# shrink towards a target vector which may not be zeros
-function ridge(y, x, k, target)
-    # standardize regressors (except the constant)
-    s = std(x,dims=1)
-    for i = 1:size(x,2)
-        if s[i]==0
-            s[i] = 1.0
-        end
-    end    
-    x = x ./ s
-    A = eye(size(x,2))
-    A[1,1] = 0.0
-    β = (inv(x'x + k*eye(size(x,2)))*[x'y + k.*target]) ./ s'
-end
