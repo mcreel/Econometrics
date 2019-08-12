4de493575ec3e46b0c8fc9b412aea4e1de3a2f66
Michael Creel <michael.creel@uab.es>
2019-08-12 11:32:57 +0200

    adjust ridge

--- src/LinearRegression/ridge.jl
+++ src/LinearRegression/ridge.jl
@@ -1,15 +1,15 @@
 function ridge(y, x, k)
     # standardize regressors (except the constant)
     s = std(x,dims=1)
+    A = eye(size(x,2))
     for i = 1:size(x,2)
         if s[i]==0
             s[i] = 1.0
+            A[i,i] = 0.0 # don't shrink constant
         end
     end    
     x = x ./ s
-    A = eye(size(x,2))
-    A[1,1] = 0.0
-    βhat = (inv(x'x + k*eye(size(x,2)))*x'y) ./ s'
+    βhat = (inv(x'x + k*A)*x'y) ./ s'
 end
 
 
