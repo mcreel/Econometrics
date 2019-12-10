06f31418bd6a7a60567888b20121fa566c0cb63f
Michael Creel <michael.creel@uab.es>
2019-12-10 11:04:50 +0100

    using PrettyTables

--- src/Utilities/PrintEstimationResults.jl
+++ src/Utilities/PrintEstimationResults.jl
@@ -1,17 +1,18 @@
 using PrettyTables
 function PrintEstimationResults(results, names)
-    clabels = ["estimate", "st. err", "t-stat", "p-value"]
+    results = [names[:] results]
+    clabels = ["parameter", "estimate", "st. err", "t-stat", "p-value"]
     h1 = Highlighter(
-                      (results, j) -> results[j,4] .< 0.01,
+                      (results, j) -> results[j,5] .< 0.01,
                       foreground = :green
                      );
     h5 = Highlighter(
-                      (results, j) -> (results[j,4] .< 0.05) & (results[j,4] .> 0.01) ,
+                      (results, j) -> (results[j,5] .< 0.05) & (results[j,4] .> 0.01) ,
                       foreground = :blue
                      );
     h10 = Highlighter(
-                       (results, j) -> (results[j,4] .< 0.1) & (results[j,4] .> 0.05) ,
+                       (results, j) -> (results[j,5] .< 0.1) & (results[j,4] .> 0.05) ,
                       foreground = :yellow
                      );
-    prettyprint(results, clabels, names; highlighters=(h1, h5, h10))
+    pretty_table(results, clabels; highlighters=(h1, h5, h10))
 end
