13e7fbd72ef0a771dd357c918206c65be5899196
Michael Creel <michael.creel@uab.es>
2019-12-10 11:30:56 +0100

    using PrettyTables

--- src/Utilities/PrintEstimationResults.jl
+++ src/Utilities/PrintEstimationResults.jl
@@ -2,5 +2,5 @@ using PrettyTables
 function PrintEstimationResults(results, names)
     results = [names[:] results]
     clabels = ["parameter", "estimate", "st. err", "t-stat", "p-value"]
-    pretty_table(results, clabels)
+    pretty_table(results, clabels; formatter=ft_printf("%12.5f"))
 end
