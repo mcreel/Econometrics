a2f623af835083d80dde5e3cd9f22e4a80d19c2b
Michael Creel <michael.creel@uab.es>
2020-01-29 09:20:48 +0100

    add IQR to dstats

--- src/Utilities/dstats.jl
+++ src/Utilities/dstats.jl
@@ -12,12 +12,13 @@ function dstats(x, rnames="";short=false)
     mn = minimum(x,dims=1)
     mx = maximum(x,dims=1)
     q05 = fill(0.0,k)
+    q25 = fill(0.0,k)
+    q75 = fill(0.0,k)
     q95 = fill(0.0,k)
     if short == false
-        for i = 1:size(x,2) q05[i] = quantile(x[:,i], 0.05) end
-        for i = 1:size(x,2) q95[i] = quantile(x[:,i], 0.95) end
-        cnames = ["  mean", " median","  std", "min", "max", "q05", "q95"]
-        stats = [m' mm' s' mn' mx' q05 q95] 
+        for i = 1:size(x,2) q05[i], q25[i], q75[i],q95[i] = quantile(x[:,i], [0.05,0.25,0.75,0.95]) end
+        cnames = ["  mean", " median","  std", "IQR", "min", "max", "q05", "q95"]
+        stats = [m' mm' s' (q75-q25)' mn' mx' q05 q95] 
         prettyprint(stats, cnames, rnames)
     else
         cnames = ["  mean", " median", "  std", "min", "max"]
@@ -31,7 +32,7 @@ end
 function dstats()
     a = [1;2;3]
     b = dstats(a)[:];
-    c = [2.0;2.0;1.0;1.0;3.0;1.1;2.9]
+    c = [2.0;2.0;1.0;1.0;1.0;3.0;1.1;2.9]
     b == c
 end    
 
