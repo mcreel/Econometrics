ca07455c79abf5d431382f903687401a3e456591
Michael Creel <michael.creel@uab.es>
2020-04-14 11:00:58 +0200

    v0.9 prettytables.jl

--- src/Utilities/PrintEstimationResults.jl
+++ src/Utilities/PrintEstimationResults.jl
@@ -5,5 +5,5 @@ function PrintEstimationResults(results, names)
     h10 = Highlighter((results,i,j) -> (j==5) && (results[i,5] < 0.1), crayon"yellow");
     h5 = Highlighter((results,i,j) -> (j==5) && (results[i,5] < 0.05), crayon"blue");
     h1 = Highlighter((results,i,j) -> (j==5) && (results[i,5] < 0.01), crayon"green");
-    pretty_table(results, clabels; formatter=ft_printf("%12.5f"), highlighters=(h1, h5, h10))
+    pretty_table(results, clabels; formatters=ft_printf("%12.5f"), highlighters=(h1, h5, h10))
 end
