8b5879c069ea4952e6301f95efec873892913739
Michael Creel <michael.creel@uab.es>
2020-05-30 08:09:09 +0200

    initialization of ma.jl

--- src/Utilities/ma.jl
+++ src/Utilities/ma.jl
@@ -1,6 +1,6 @@
 # compute moving average using p most recent values, including current value
 function ma(x, p)
-    m = similar(x)
+    m = zeros(size(x))
     for i = p:size(x,1)
         m[i] = mean(x[i-p+1:i])
     end
