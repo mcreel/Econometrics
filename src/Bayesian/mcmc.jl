4a7e92c970ef6fe8a309ef84cc2b6960a8276289
Michael Creel <michael.creel@uab.es>
2020-07-23 09:21:38 +0200

    fix

--- src/Bayesian/mcmc.jl
+++ src/Bayesian/mcmc.jl
@@ -72,7 +72,7 @@ end
 
 
 # method using threads and symmetric proposal
-function mcmc(θ::Array{Float64}, reps::Int64, burnin::Int64, Prior::Function, lnL::Function, Proposal::Function, report=true::Bool, nthreads::Int)
+function mcmc(θ::Array{Float64}, reps::Int64, burnin::Int64, Prior::Function, lnL::Function, Proposal::Function, report=true::Bool, nthreads=1::Int)
     perthread = Int(round(reps/nthreads))
     chain = zeros(reps, size(θ,1)+1)
     Threads.@threads for t = 1:nthreads # collect the results from the threads
