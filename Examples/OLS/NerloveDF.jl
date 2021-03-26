508aee681ca42ff1f361fd48cd64de6565ece221
Michael Creel <michael.creel@uab.es>
2021-03-26 16:50:15 +0100

    fix Nerlove example using DataFrame and GLM

--- Examples/OLS/NerloveDF.jl
+++ Examples/OLS/NerloveDF.jl
@@ -1,7 +1,6 @@
 # shows some data analysis using DataFrames
-using CSV, DataFrames, GLM
-dir = dirname(dirname(pathof(Econometrics)))
-nerlove = CSV.read(dir*"/Examples/Data/nerlove.csv")
+using Econometrics, CSV, DataFrames, GLM
+nerlove = DataFrame(CSV.File("../Data/nerlove.csv"))
 f = @formula(log(cost) ~ 1 + log(output) + log(labor) + log(fuel) + log(capital))
 println()
 println("using the GLM package")
