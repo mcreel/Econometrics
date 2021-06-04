7b77f09adbc13cf62f804df1bb160f826d01a144
Michael Creel <michael.creel@uab.es>
2021-06-04 15:42:47 +0200

    lags

--- src/Utilities/lag.jl
+++ src/Utilities/lag.jl
@@ -2,11 +2,8 @@
 # with the first p rows filled with ones (to avoid divide errors)
 # remember to drop those rows before doing analysis
 function lag(x,p)
-	n,k = size(x)
+	n = size(x,1)
+    k = size(x,2)
 	lagged_x = [ones(p,k); x[1:n-p,:]]
 end
 
-function lag(x,p)
-	n = size(x,1)
-	lagged_x = [ones(p); x[1:n-p]]
-end	 
