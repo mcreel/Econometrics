8345fdbf02c4835999f657a493a5e64922aa52f8
Michael Creel <michael.creel@uab.es>
2021-06-04 14:25:35 +0200

    remove type from lags

--- src/Utilities/lags.jl
+++ src/Utilities/lags.jl
@@ -1,7 +1,7 @@
 # returns the variable (or matrix), lagged from 1 to p times,
 # with the first p rows filled with ones (to avoid divide errors)
 # remember to drop those rows before doing analysis
-function  lags(x::Array{Float64,2},p)
+function  lags(x,p)
 	n, k = size(x)
 	lagged_x = zeros(eltype(x),n,p*k)
 	for i = 1:p
@@ -10,7 +10,7 @@ function  lags(x::Array{Float64,2},p)
     return lagged_x
 end	
 
-function  lags(x::Array{Float64,1},p)
+function  lags(x,p)
 	n = size(x,1)
 	lagged_x = zeros(eltype(x), n,p)
 	for i = 1:p
