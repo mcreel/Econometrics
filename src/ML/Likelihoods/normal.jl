15bfb9d0d1d381b74c0917e638f5923af5fc9b93
Michael Creel <michael.creel@uab.es>
2021-08-27 10:21:42 +0200

    fixes

--- src/ML/Likelihoods/normal.jl
+++ src/ML/Likelihoods/normal.jl
@@ -3,8 +3,8 @@
 # the parameter theta = [b' s]
 # this file is used for demonstration of MLE, to compare to OLS
 function normal(theta, y, x)
-    b = theta[1:end-1][1]
-    s = theta[end][1]
+    b = theta[1:end-1]
+    s = theta[end]
     e = (y - x*b)./s
     logdensity = -log.(sqrt.(2.0*pi)) .- 0.5*log(s.^2) .- 0.5*e.*e
 end
