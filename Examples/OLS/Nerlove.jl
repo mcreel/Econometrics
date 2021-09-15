c33e22973d72c956e4701ff6eecb88fa66a0e34a
Michael Creel <michael.creel@uab.es>
2021-09-15 17:43:43 +0200

    a few updates

--- Examples/OLS/Nerlove.jl
+++ Examples/OLS/Nerlove.jl
@@ -9,7 +9,7 @@ y = data[:,1]
 x = data[:,2:end]
 x = [ones(n,1) x]
 names = ["constant", "output", "labor", "fuel", "capital"]
-b, junk, junk, junk = ols(y, x, names=names)
+b, junk, junk, junk = ols(y, x, names=names, vc="ols")
 return
 end
 main()
