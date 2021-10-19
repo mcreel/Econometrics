ff792e437f8e9582d2af2911c2ec8157da7d9503
Michael Creel <michael.creel@uab.es>
2021-10-19 11:31:46 +0200

    minor edits

--- Examples/DSGE/GenData/GenData.jl
+++ Examples/DSGE/GenData/GenData.jl
@@ -1,8 +1,11 @@
-using SolveDSGE, StatsPlots, DelimitedFiles
+using SolveDSGE, StatsPlots, DelimitedFiles, DataFrames, CSV
 include("CKlib.jl")
 data = dgp(TrueParameters(), dsge, 1)[1]
 plot(data, legend=:outertopright, label=["output" "cons" "hours" "r" "w"])
 gui()
 #savefig("dsgedata.svg")
 #writedlm("dsgedata.txt", data)
+df = DataFrame(data, ["output", "cons", "hour","r","w"])
+#CSVwrite("dsgedata.csv", df)
+
 
