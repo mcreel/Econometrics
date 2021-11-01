b67f6c9a05e6843ff0b086bbd97fe70ffebf91ad
Michael Creel <michael.creel@uab.es>
2021-11-01 12:58:01 +0100

    go back to loggamma

--- src/ML/Likelihoods/poisson.jl
+++ src/ML/Likelihoods/poisson.jl
@@ -1,5 +1,6 @@
+using SpecialFunctions
 function  poisson(theta, y, x)
     lambda = exp.(x*theta)
     n = size(y,1)
-    logdensity = y .* (x*theta) .- lambda .- log.(factorial.(y))
+    logdensity = y .* (x*theta) .- lambda .- loggamma.(y+1)
 end
