b65cbe06f782fec64a1cfca44b94e063c502afef
Michael Creel <michael.creel@uab.es>
2022-02-24 10:49:09 +0100

    add negative binomial loglikelihood

--- src/ML/Likelihoods/negbin.jl
+++ src/ML/Likelihoods/negbin.jl
@@ -7,7 +7,7 @@ function  negbin(θ, y, x, nbtype)
     n, k = size(x)
     λ = exp.(x*θ[1:k])
     α = exp(θ[end])
-    ? nbtype == 1 ψ = λ/α : ψ = ones(n)/α
+    nbtype == 1 ? ψ = λ/α : ψ = ones(n)/α
     logdensity = loggamma.(y .+ ψ) - loggamma.(ψ) - loggamma.(y .+ 1)
                 + ψ .* log.(ψ ./ (λ + ψ)) + y .* log.(λ ./(λ + ψ)) 
 end
