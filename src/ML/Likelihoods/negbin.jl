c9c52db194308d8eda40dabc4d1e799c2e17f1a8
Michael Creel <michael.creel@uab.es>
2022-02-24 14:54:36 +0100

    add negative binomial loglikelihood

--- src/ML/Likelihoods/negbin.jl
+++ src/ML/Likelihoods/negbin.jl
@@ -6,8 +6,9 @@ using SpecialFunctions, Distributions
 function  negbin(θ, y, x, nbtype)
     n, k = size(x)
     β = θ[1:k]
-    λ = exp.(x*β)
-    α = exp(θ[end])
+    eps = 1e-8
+    λ = eps .+ exp.(x*β)
+    α = eps .+ exp(θ[end])
     nbtype == 1 ? r = α./λ : r = ones(n)/α
     p = r ./ (r + λ)
     ((r > zero(r)) & (p > zero(p)) & ((1.0 .-p) > zero(p))) ? log.(pdf.(NegativeBinomial.(r, p),y)) : -Inf    
