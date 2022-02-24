1062bdb94150455b98068b3879a6d637904b83d4
Michael Creel <michael.creel@uab.es>
2022-02-24 13:11:38 +0100

    add negative binomial loglikelihood

--- src/ML/Likelihoods/negbin.jl
+++ src/ML/Likelihoods/negbin.jl
@@ -2,15 +2,14 @@
 #  The parameterization follows
 #  Deb and Trivedi, J. Appl. Econometrics,
 #  V. 12, 1997, pp. 313 - 336.
-using SpecialFunctions
+using SpecialFunctions, Distributions
 function  negbin(θ, y, x, nbtype)
     n, k = size(x)
     β = θ[1:k]
     λ = exp.(x*β)
     α = exp(θ[end])
     nbtype == 1 ? ψ = λ/α : ψ = ones(n)/α
-    logdensity = loggamma.(y .+ ψ) - loggamma.(ψ) - loggamma.(y .+ 1.0)
-                + ψ .* log.(ψ ./ (λ + ψ)) + y .* log.(λ ./(λ + ψ)) 
+    logdensity = log.(pdf.(NegativeBinomial(ψ, ψ./(ψ + λ))))    
 end
 
 
