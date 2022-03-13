33a7f3010644eba0f166b8e6b9d2a7f1ad8e1f31
Michael Creel <michael.creel@uab.es>
2022-03-13 12:10:42 +0100

    add Turing and example

---
+++ Examples/Bayesian/BayesExample3.jl
@@ -0,0 +1,20 @@
+# sample model as in BayesExamples1 and 2, but using
+# a specialized package and more sophisticated sampling
+using Distributions, Turing, StatsPlots
+function main()
+# sample is from exponential, prior is lognormal
+y = rand(Exponential(3.0), 30)
+# the model: prior and likelihood
+@model function ExpModel(y)
+    θ ~ LogNormal(1.,1.)
+    y ~ Exponential(θ)
+end    
+# get the chain
+Proposal = θ -> rand(LogNormal(log(θ),tuning))
+chain = sample(ExpModel(y), NUTS(200, 0.65), 10000)
+end
+chain = main()
+# examine and plot chain
+display(chain)
+plot(chain[end-1000:end])
+
