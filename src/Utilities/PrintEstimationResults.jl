3689b28bc428a9262e6d990be2cbe4d584747d1b
Michael Creel <michael.creel@uab.es>
2022-03-22 10:19:11 +0100

    try term

--- src/Utilities/PrintEstimationResults.jl
+++ src/Utilities/PrintEstimationResults.jl
@@ -1,5 +1,5 @@
 using PrettyTables, Term
-function PrintEstimationResults(results, names, title="")
+function PrintEstimationResults(results, names)
     results = [names[:] results]
     clabels = ["parameter", "estimate", "st. err", "t-stat", "p-value"]
     h10 = Highlighter((results,i,j) -> (j==5) && (results[i,5] < 0.1), crayon"yellow");
@@ -7,5 +7,5 @@ function PrintEstimationResults(results, names, title="")
     h1 = Highlighter((results,i,j) -> (j==5) && (results[i,5] < 0.01), crayon"green");
     Panel(
         pretty_table(results; header=clabels, formatters=ft_printf("%12.5f"), highlighters=(h1, h5, h10)),
-    width=80, justify=:center, style="blue", box=:DOUBLE, title=title, title_style="white")
+    width=80, justify=:center, style="blue", box=:DOUBLE, title="Estimation results", title_style="white")
 end
