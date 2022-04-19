71cf790487ee1fa1ce098f1842cb236913f3377a
Michael Creel <michael.creel@uab.es>
2022-04-19 10:16:40 +0200

    experiments with plot backends

--- PracticalSummaries/12-Optimization.jl
+++ PracticalSummaries/12-Optimization.jl
@@ -59,10 +59,11 @@ f(x,y) = -(3*(1-x)^2 * exp(-(x^2) - (y+1)^2)
  
 ##
 # let's explore it:
-using Pkg
+#using Pkg
 #Pkg.add("PlotlyJS") # this one is not in the system image
 using Plots
 #plotlyjs() # use this backend for interactive plots
+plotly()
 x = range(-4, step=0.1, stop=4)
 y = x
 surface(x, y, (x,y)->f(x,y),c=:viridis)
