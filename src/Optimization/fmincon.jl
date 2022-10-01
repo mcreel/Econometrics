9a37b2fea6610dcd4098f783b94d35d02e032d93
Michael Creel <michael.creel@uab.es>
2022-10-01 06:34:08 +0200

    update deps and test with 1.8.2

--- src/Optimization/fmincon.jl
+++ src/Optimization/fmincon.jl
@@ -8,6 +8,58 @@ fminunc() uses NLopt.jl to do the actual minimization.
 
 """
 function fmincon(obj, startval, R=[], r=[], lb=[], ub=[]; tol = 1e-10, iterlim=0)
+ e   # the objective is an anonymous function
+    function objective_function(x::Vector{Float64}, grad::Vector{Float64})
+        obj_func_value = obj(x)[1,1]
+        return(obj_func_value)
+    end
+    # impose the linear restrictions
+    function constraint_function(x::Vector, grad::Vector, R, r)
+        result = R*x .- r
+        return result[1,1]
+    end
+    opt = Opt(:LN_COBYLA, size(startval,1))
+    min_objective!(opt, objective_function)
+    # impose lower and/or upper bounds
+    if lb != [] lower_bounds!(opt, lb) end
+    if ub != [] upper_bounds!(opt, ub) end
+    # impose linear restrictions, by looping over the rows
+    if R != []
+        for i = 1:size(R,1)
+            equality_constraint!(opt, (theta, g) -> constraint_function(theta, g, R[i:i,:], r[i]), tol)
+        end
+    end    
+    xtol_rel!(opt, tol)
+    ftol_rel!(opt, tol)
+    maxeval!(opt, iterlim)
+    (objvalue, xopt, flag) = NLopt.optimize(opt, startval)
+    return xopt, objvalue, flag
+   # the objective is an anonymous function
+    function objective_function(x::Vector{Float64}, grad::Vector{Float64})
+        obj_func_value = obj(x)[1,1]
+        return(obj_func_value)
+    end
+    # impose the linear restrictions
+    function constraint_function(x::Vector, grad::Vector, R, r)
+        result = R*x .- r
+        return result[1,1]
+    end
+    opt = Opt(:LN_COBYLA, size(startval,1))
+    min_objective!(opt, objective_function)
+    # impose lower and/or upper bounds
+    if lb != [] lower_bounds!(opt, lb) end
+    if ub != [] upper_bounds!(opt, ub) end
+    # impose linear restrictions, by looping over the rows
+    if R != []
+        for i = 1:size(R,1)
+            equality_constraint!(opt, (theta, g) -> constraint_function(theta, g, R[i:i,:], r[i]), tol)
+        end
+    end    
+    xtol_rel!(opt, tol)
+    ftol_rel!(opt, tol)
+    maxeval!(opt, iterlim)
+    (objvalue, xopt, flag) = NLopt.optimize(opt, startval)
+    return xopt, objvalue, flag
     # the objective is an anonymous function
     function objective_function(x::Vector{Float64}, grad::Vector{Float64})
         obj_func_value = obj(x)[1,1]
