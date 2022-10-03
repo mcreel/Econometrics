7a5c4039ecdcf8521c7abab745374eb3326b9990
Michael Creel <michael.creel@uab.es>
2022-10-03 10:18:53 +0200

    extend ch22 practical example

--- PracticalSummaries/22-SimulationBased.jl
+++ PracticalSummaries/22-SimulationBased.jl
@@ -5,20 +5,22 @@
 #  difficult. It goes on to explore Bayesian
 #  MSM, which is pretty easy to implement.
 #  
-#  The second is for SML, and shows how chatter can
-#  be controlled, but that simulation can introduce
-#  discontinuities in the criterion. This can also
+#  The second is for SML, and shows how simulated
+#  discrete dependent variable models can suffer 
+#  from discontinuities in the criterion. This can also
 #  be dealt with using the Bayesian approach.
 
+
+########################### MSM Example ###############################
 using Term
 println(@yellow "Start of MSM example")
-#  MSM exampl. We generate data that follows a Poisson
+#  MSM exampl. We generate data that follows a Gaussian
 #  distribution, and estimate using some
 #  simple simulated moments
-
-# the standard parameterization of Poisson density
-function λ(x,θ)
-    exp.(x*θ)
+#
+# conditional mean is usual linear function
+function μ(x,θ)
+    x*θ
 end
 
 ## generate the "true" data
@@ -26,21 +28,25 @@ using Distributions
 n = 100
 x = randn(n,2)
 θ₀ = [1.,1.] # true params
-y = rand.(Poisson.(λ(x,θ₀)))
+y = rand.(Normal.(μ(x,θ₀),1.0))
 
 ## the statistics, averaged over observations
 function k(x,y)
-    vec(mean([y sqrt.(y) x.*y x.*sqrt.(y)], dims=1))
+    vec(mean([y x.*y x.*(y.^2.0)], dims=1))
 end
 
 ## the simulated moments: S replications
-function simulated_moments(θ, S, x)
-    [k(x, rand.(Poisson.(λ(x,θ)))) for _ = 1:S]
+using Random
+function simulated_moments(θ, x, S, controlchatter)
+    controlchatter ? Random.seed!(1234) : nothing
+    [k(x, rand.(Normal.(μ(x,θ),1.0))) for _ = 1:S]
 end
 
 ## Define the MSM criterion 
-S = 20 # number of simulation replications
-m = θ -> k(x,y) - mean(simulated_moments(θ, S, x)) 
+# IMPORTANT: try running this with chatter, and without.
+S = 100 # number of simulation replications
+controlchatter = false
+m = θ -> k(x,y) - mean(simulated_moments(θ, x, S, controlchatter)) 
  # sums of squares of moments: corresponds to GMM with identity weight
 function obj(θ)
     mean(abs2, m(θ))
@@ -52,16 +58,20 @@ end
 ## profile the MSM objective function
 using Plots
 gr()
-θ₁ = range(0.6, length=100, stop=1.2)
+θ₁ = range(0., length=100, stop=2.0)
 θ₂ = θ₁
 p2 = contour(θ₁, θ₂, (θ₁,θ₂)->obj(θ₁,θ₂),c=:viridis)
 xlabel!("x")
 ylabel!("y")
-## The objective function looks horrible!
+## The objective function looks horrible, when there is chatter
 #  Nevertheless, let's attempt to do gradient-based estimation
 using Econometrics
+# run this next line repeatedly, using CTRL-enter. When chatter
+# is controlled, you always get the same result. When it's not,
+# the result changes every time.
 fminunc(obj, zeros(2))
-println(@red "Bzzzt!")
+println(@green "You should find that MSM works, when chatter is controlled")
+println(@yellow "(Remember that the true parameters are $θ₀)")
 # note that theta hat is same as start values, it didn't work
 
 ## Now, let's do Bayesian MSM, as suggested by
@@ -72,7 +82,7 @@ k_data = k(x,y) # stats from the real data
 @model function MSM(k_data, S, x)
     θ ~ arraydist([Normal() for _=1:2]) # the prior (note: it's biased)
     # sample from the model, at the trial parameter value, and compute statistics
-    ks = simulated_moments(θ, S, x)
+    ks = simulated_moments(θ, x, S, false)
     kbar = mean(ks) # mean of simulated statistics, over S replications
     Σ = cov(ks)  # estimated covariance of statistics
     k_data ~ MvNormal(kbar, Σ) # this corresponds to CUE GMM, but adding the determinant from the MVN 
@@ -82,7 +92,7 @@ end
 # look at acceptance rate, below, and adjust
 # the tuning to make acceptance around 0.25
 S = 100 # make this as large as possible, given computation time, when doing real research
-tuning = 0.01 
+tuning = 0.05
 length = 10000
 burnin = 1000
 chain = sample(MSM(k_data, S, x),
@@ -94,7 +104,11 @@ corner(chain)
 c = Array(chain)
 acceptance = size(unique(c[:,1]),1)[1] / size(c,1)
 println("Acceptance rate: $acceptance")
+println(@red "you want to see acceptance around 0.2, and ESS above 200")
+println(@red "Increase S and adjust tuning to achieve that. Try S=100, tuning=0.05")
 println(@cyan "End of MSM example")
+
+########################### SML Example ###############################
 
 ## Now an SML example, using a simple Logit model,
 #  where there is no actual need for simulation. 
@@ -148,7 +162,7 @@ ylabel!("y")
 #  Nevertheless, let's attempt to do gradient-based estimation
 using Econometrics
 display(fminunc(smlobj, zeros(2)))
-println(@red "That didn't work!")
+println(@red "Bzzzt! That didn't work!")
 
 ## Now, let's do Bayesian SML, again using 
 #  methods from Chernozhukov and Hong (2003)
