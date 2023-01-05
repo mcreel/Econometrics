9c7519abfe8d087ce4ef80abcf3fc9567bb49299
Michael Creel <michael.creel@uab.es>
2023-01-05 09:54:24 +0100

    snap

--- Examples/GLS/CO2Example.jl
+++ Examples/GLS/CO2Example.jl
@@ -1,4 +1,5 @@
 using Econometrics, DelimitedFiles, Plots
+cd(@__DIR__)
 CO2 = readdlm("../Data/CO2.data")
 plot(CO2)
 n = size(CO2,1)
@@ -6,7 +7,7 @@ trend = 1:n
 x = [ones(n) trend]
 b, junk, e, junk, junk= ols(CO2,x)
 fit = x*b
-plot(trend[end-36:end], e[end-36:end], legend=false)
+p = plot(trend[end-36:end], e[end-36:end], legend=false)
 title!("OLS residuals, last 3 years of data")
-savefig("CO2Residuals.png")
-gui()
+display(p)
+#savefig("CO2Residuals.png")
