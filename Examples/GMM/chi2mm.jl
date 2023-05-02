28fef9677a3859af7fe1cd3a15afbb70ab64c1fb
Michael Creel <michael.creel@uab.es>
2023-05-02 20:10:05 +0200

    deps, etc.

--- Examples/GMM/chi2mm.jl
+++ Examples/GMM/chi2mm.jl
@@ -2,19 +2,22 @@
 # increase n to observe consistency
 # note that the two estimators are different from one another
 ##
-using Distributions
-n = 30
+using Distributions, StatsPlots
+n = 10000
 theta = 3
 
 # the distribution doesn't matter, what matters is
 # that the moments are correctly specified
-#y = rand(Normal(theta, sqrt(2*theta)), n)
+#y = rand(Normal(theta, sqrt(2*theta)), n) # this Normal function takes μ and σ
 y = rand(Chisq(theta), n)
+display(density(y))
 
 # a MM estimator
 thetahat = mean(y)
 println("mm estimator based on mean: ", thetahat)
+vline!([thetahat])
 
 # a second MM estimator
 thetahat = 0.5*var(y)
 println("mm estimator based on variance: ", thetahat)
+vline!([thetahat])
