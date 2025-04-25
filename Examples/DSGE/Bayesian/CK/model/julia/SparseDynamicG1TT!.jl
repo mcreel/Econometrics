629bd37151319e37701f244d0efed53e4d7bb39f
Michael Creel <michael.creel@uab.es>
2025-04-25 10:34:15 +0200

    update dynare, fix Optim deprecations

--- Examples/DSGE/Bayesian/CK/model/julia/SparseDynamicG1TT!.jl
+++
@@ -1,9 +0,0 @@
-function SparseDynamicG1TT!(T::Vector{<: Real}, y::Vector{<: Real}, x::Vector{<: Real}, params::Vector{<: Real}, steady_state::Vector{<: Real})
-    SparseDynamicResidTT!(T, y, x, params, steady_state)
-@inbounds begin
-T[5] = get_power_deriv(y[14],params[1],1)
-T[6] = get_power_deriv(y[15],1-params[1],1)
-end
-    return nothing
-end
-
--- Examples/DSGE/ML/ML/model/julia/SparseDynamicG2TT!.jl
+++
@@ -1,9 +0,0 @@
-function SparseDynamicG2TT!(T::Vector{<: Real}, y::Vector{<: Real}, x::Vector{<: Real}, params::Vector{<: Real}, steady_state::Vector{<: Real})
-    SparseDynamicG1TT!(T, y, x, params, steady_state)
-@inbounds begin
-T[9] = get_power_deriv(y[15],1-params[1],2)
-T[10] = get_power_deriv(y[14],params[1],2)
-end
-    return nothing
-end
-
