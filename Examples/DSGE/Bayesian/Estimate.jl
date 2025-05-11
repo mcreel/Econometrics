b36cbe873b2b45a209ce136ef09306c98f05ed0d
Michael Creel <michael.creel@uab.es>
2025-05-11 17:48:57 +0200

    use specific project for Dynare

--- Examples/DSGE/Bayesian/Estimate.jl
+++ Examples/DSGE/Bayesian/Estimate.jl
@@ -3,4 +3,4 @@ cd(@__DIR__)
 context = @dynare "CK.mod" ; 
 
 chain = deserialize("CK/output/mcmc_chain_1.jls")
-mean(chain.value[:,1:end-1],dims=1)
+run(`./cleanup`)
--- Examples/DSGE/ML/Estimate.jl
+++ Examples/DSGE/ML/Estimate.jl
@@ -3,4 +3,4 @@ using Dynare
 cd(@__DIR__)
 
 context = @dynare "ML.mod" ;
-; # don't want to see all that output
+run(`./cleanup`);
